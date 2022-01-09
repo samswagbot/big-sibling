a46292f8584fe2518ec32e9aa92836ec7fc85cb6
Samson <sammossallam@gmail.com>
2022-01-09 00:03:28 -0500

    comment out prodConifig

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@ import { getFirestore } from "firebase/firestore";
 // As stated by Firebase it is safe to expose your firebase config, secutiry rules
 // are what protext your db https://medium.com/@paulbreslin/is-it-safe-to-expose-your-firebase-api-key-to-the-public-7e5bd01e637b 
 
-const prodConfig = {
-  apiKey: "AIzaSyDWhS1bktY9GbE2d5Gg1BFPVe0IYF1Dkwo",
-  authDomain: "big-sibling-production.firebaseapp.com",
-  projectId: "big-sibling-production",
-  storageBucket: "big-sibling-production.appspot.com",
-  messagingSenderId: "685425871508",
-  appId: "1:685425871508:web:d7c2444befeb60bdbf4281",
-  measurementId: "G-PFZYGWPW0T",
-};
+// const prodConfig = {
+//   apiKey: "AIzaSyDWhS1bktY9GbE2d5Gg1BFPVe0IYF1Dkwo",
+//   authDomain: "big-sibling-production.firebaseapp.com",
+//   projectId: "big-sibling-production",
+//   storageBucket: "big-sibling-production.appspot.com",
+//   messagingSenderId: "685425871508",
+//   appId: "1:685425871508:web:d7c2444befeb60bdbf4281",
+//   measurementId: "G-PFZYGWPW0T",
+// };
 
 const devConfig = {
   apiKey: "AIzaSyBgsW9fUXT42opku7scbhex-8_HwRNEi0s",
