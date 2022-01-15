c710f8e4511ca92a15ae59b107d819507c739e93
Samson <sammossallam@gmail.com>
2022-01-14 23:04:46 -0500

    switch for env

--- src/firebase.js
+++ src/firebase.js
@@ -8,22 +8,24 @@ const prodConfig = {
   apiKey: "AIzaSyDWhS1bktY9GbE2d5Gg1BFPVe0IYF1Dkwo",
   authDomain: "big-sibling-production.firebaseapp.com",
   projectId: "big-sibling-production",
-  // storageBucket: "big-sibling-production.appspot.com",
-  // messagingSenderId: "685425871508",
-  // appId: "1:685425871508:web:d7c2444befeb60bdbf4281",
-  // measurementId: "G-PFZYGWPW0T"
+  storageBucket: "big-sibling-production.appspot.com",
+  messagingSenderId: "685425871508",
+  appId: "1:685425871508:web:d7c2444befeb60bdbf4281",
+  measurementId: "G-PFZYGWPW0T",
 };
 
-// const devConfig = {
-//   apiKey: "AIzaSyBgsW9fUXT42opku7scbhex-8_HwRNEi0s",
-//   authDomain: "mentorship-app-b1b29.firebaseapp.com",
-//   databaseURL: "https://mentorship-app-b1b29.firebaseio.com",
-//   projectId: "mentorship-app-b1b29",
-//   storageBucket: "mentorship-app-b1b29.appspot.com",
-//   messagingSenderId: "860290522386",
-//   appId: "1:860290522386:web:bb8f28ab251f9e55",
-// };
+const devConfig = {
+  apiKey: "AIzaSyBgsW9fUXT42opku7scbhex-8_HwRNEi0s",
+  authDomain: "mentorship-app-b1b29.firebaseapp.com",
+  databaseURL: "https://mentorship-app-b1b29.firebaseio.com",
+  projectId: "mentorship-app-b1b29",
+  storageBucket: "mentorship-app-b1b29.appspot.com",
+  messagingSenderId: "860290522386",
+  appId: "1:860290522386:web:bb8f28ab251f9e55",
+};
 
-export const app = initializeApp(prodConfig);
+export const app = initializeApp(
+  process.env.NODE_ENV === "production" ? prodConfig : devConfig
+);
 
 export const db = getFirestore(app);
